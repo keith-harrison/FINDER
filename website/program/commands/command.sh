ad85a6e51c37f954ba5edc2b654ebe24d2dd41c1
Keith Harrison <keithharrison2000@gmail.com>
2021-03-26 15:32:23 +0000

    Update command.sh

--- website/program/commands/command.sh
+++ website/program/commands/command.sh
@@ -9,6 +9,7 @@ fasterq-dump @SRRNUMBER @SRRNUMBER -O ${PWD}
 #debate whether or not to remove splitting on large files on AWS
 split -b 80G -d @SRRNUMBER.fastq @SRRNUMBER.fastq
 
+#Removes paired reads as there is now an intertwined file
 rm @SRRNUMBER_*.fastq
 #RENAME FILES TO BE NICER
 ls *.fastq0*| cat -n | while read n f; do mv -n "$f" "$n"@SRRNUMBER.fastq; done 
@@ -42,10 +43,10 @@ multiqc -n beforetrimmingquality ./*_fastqc.zip
 find . -name "*_fastqc*" -type f -delete 
 
 #PERFORM CUTADAPT ON DEFAULT SETTINGS
-ls *.fastq| cat | while read -r line; do cutadapt --cores=4 -o trimmed"$line" "$line"  ; done
+ls 1*.fastq| cat | while read -r line; do cutadapt --cores=4 -o trimmed"$line" "$line"  ; done
 
 #GENERATE QUALITY ANALYSIS FOR FILES POST TRIMMING
-fastqc trimmed*.fastq*
+fastqc trimmed1*.fastq*
 
 multiqc -n trimmedquality ./*_fastqc.zip
 find . -name "*_fastqc*" -type f -delete 
@@ -53,14 +54,14 @@ find . -name "*_fastqc*" -type f -delete
 #PERFORM BOWTIE2 ALIGNMENT TO LOOK AT COVERAGE
 bowtie2-build mergedreference.fasta refgenome
 
-ls trimmed*.fastq| cat | while read -r line; do bash ./commands/bowtiecoverage.sh "$line"   ; done
+ls trimmed1*.fastq| cat | while read -r line; do bash ./commands/bowtiecoverage.sh "$line"   ; done
 
 
 if test -f "/work/assemble.txt"; then
     #PERFORM ASSEMBLY BY ragtag USING TRUSTED CONTIGS FROM REFERENCE
-    ls trimmed*.fastq| cat | while read -r line; do python3 /root/miniconda3/bin/ragtag.py scaffold -o "$line".d --aligner "/root/miniconda3/bin/minimap2" mergedreference.fasta "$line" ; done
+    ls trimmed1*.fastq| cat | while read -r line; do python3 /root/miniconda3/bin/ragtag.py scaffold -o "$line".d --aligner "/root/miniconda3/bin/minimap2" mergedreference.fasta "$line" ; done
     #perform assembly by RagTag
-    ls trimmed*@SRRNUMBER.fastq.d/ragtag.scaffolds.fasta | cat | while read -r line; do bash ./commands/bowtiecoverage2.sh "$line"   ; done
+    ls trimmed1*@SRRNUMBER.fastq.d/ragtag.scaffolds.fasta | cat | while read -r line; do bash ./commands/bowtiecoverage2.sh "$line"   ; done
 
 
 fi
@@ -87,7 +88,7 @@ mv ftp_folder.txt ${PWD}/@TITLE
 mv @SRRNUMBER_info.csv ${PWD}/@TITLE
 mv mapping_result_sorted.bam ${PWD}/@TITLE && mv depth.png ${PWD}/@TITLE 
 #PERFORM QUAST
-[ -r /work/assemble.txt ] && ls ${PWD}/@TITLE/trimmed*.fastq| cat | while read -r line; do python3 /quast-quast_5.1.0rc1/quast.py -R ${PWD}/@TITLE/mergedreference.fasta "$line".d/ragtag.scaffolds.fasta -o "$line"referencereport   ; done
+[ -r /work/assemble.txt ] && ls ${PWD}/@TITLE/trimmed1*.fastq| cat | while read -r line; do python3 /quast-quast_5.1.0rc1/quast.py -R ${PWD}/@TITLE/mergedreference.fasta "$line".d/ragtag.scaffolds.fasta -o "$line"referencereport   ; done
 [ -r /work/assemble.txt ] && mv mapping_result_sorted2.bam ${PWD}/@TITLE && mv depth2.png ${PWD}/@TITLE 
 rm /work/assemble.txt
 
